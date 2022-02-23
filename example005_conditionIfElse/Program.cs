fd6157e7cc56e312f2a46cdbd806d4c7255cb639
Anya <d05412022@gmail.com>
2022-02-23 22:35:39 +0300

    example005 condition if/else

---
+++ example005_conditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("enter ur name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "pipita")
+{
+    Console.WriteLine("Pipi is the most beautiful girl in tne world");
+}
+else
+{
+    Console.WriteLine ("hi, ");
+    Console.WriteLine (username);
+}
